5823bfc4b8abab31c48b1d166cec2e21000a420c
Chocolaterie <110991127+Chocolaterie@users.noreply.github.com>
2024-06-04 10:32:45 +0200

    tp 3

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // import express
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 
 // instancier le serveur
 const app = express();
@@ -7,24 +8,42 @@ const app = express();
 // pour autoriser l'envoie des données dans le body 
 app.use(express.json());
 
-// Simulation de données en mémoire
-let DB_ARTICLES = [
-    { id: 1, title: 'Premier article', content: 'Contenu du premier article', author: 'Isaac' },
-    { id: 2, title: 'Deuxième article', content: 'Contenu du deuxième article', author: 'Sanchez' },
-    { id: 3, title: 'Troisième article', content: 'Contenu du troisième article', author: 'Toto' }
-];
+// ----------------------------------------------------------
+// * MongoDB
+// ----------------------------------------------------------
+const mongoose = require('mongoose');
 
-// Routes
-app.get('/articles', (request, response) => {
-    return response.json(DB_ARTICLES);
+// Si connexion reussie
+mongoose.connection.once('open', () => {
+    console.log(`Connecté(e) à la base de données`);
 });
 
-app.get('/article/:id', (request, response) => {
-    // Recupérer l'id dans l'url
-    const idParam = parseInt(request.params.id);
+// Si erreur bdd
+mongoose.connection.on('error', (err) => {
+    console.log(`Erreur à la base données`);
+});
 
-    // Trouver un article dans la liste des articles selon l'id
-    const foundArticle = DB_ARTICLES.find(article => article.id === idParam);
+// Enclencher à la connexion
+mongoose.connect('mongodb://127.0.0.1:27017/db_article');
+
+// Model Article
+const Article = mongoose.model('Article', { id: String, title : String, content : String, author : String }, 'articles');
+
+// Routes
+app.get('/articles', async (request, response) => {
+
+    // récupérer les articles en base
+    const articles = await Article.find();
+
+    return response.json(articles);
+});
+
+app.get('/article/:id', async (request, response) => {
+    // Recupérer l'id dans l'url
+    const idParam = request.params.id;
+
+    // Trouver un article dans la BDD selon l'id
+    const foundArticle = await Article.findOne({ id : idParam});
 
     // retourner l'article trouvé en json
     return response.json(foundArticle);
@@ -33,21 +52,24 @@ app.get('/article/:id', (request, response) => {
 /**
  * Ajouter/modifier un article
  */
-app.post('/save-article', (request, response) => {
+app.post('/save-article', async (request, response) => {
     // Si id renseigné
     if (request.body.id){
         // Si edition
 
         // -- Recupérer l'id dans l'url
-        const idParam = parseInt(request.body.id);
+        const idParam = request.body.id;
 
         // Récupérer l'article à modifier
-        let articleToUpdate = DB_ARTICLES.find(article => article.id === idParam);
+        let articleToUpdate = await Article.findOne({ id : idParam });
 
         // modifier les valeurs (adresse mémoire donc pas besoin de modifier le tableau)
         articleToUpdate.title = request.body.title;
         articleToUpdate.content = request.body.content;
         articleToUpdate.author = request.body.author;
+
+        // sauvegarder en base
+        await articleToUpdate.save();
 
         // retourner la réponse json
         return response.json(`L'article a été modifié avec succès`);
@@ -55,11 +77,11 @@ app.post('/save-article', (request, response) => {
     // Sinon creation
     let articleToCreate = request.body;
 
-    // -- simuler auto increment
-    articleToCreate.id = DB_ARTICLES.length + 1;
+    // -- générer un id unique
+    articleToCreate.id = uuidv4();
 
     // -- ajouter dans le tableau
-    DB_ARTICLES.push(articleToCreate);
+    await Article.create(articleToCreate);
 
     // retourner la réponse json
     return response.json(`L'article a été ajouté avec succès`);
@@ -68,20 +90,20 @@ app.post('/save-article', (request, response) => {
 /**
  * Supprimer un article
  */
-app.delete('/article/:id', (request, response) => {
+app.delete('/article/:id', async (request, response) => {
     // Recupérer l'id dans l'url
-    const idParam = parseInt(request.params.id);
+    const idParam = request.params.id;
 
-    // Trouver l'index de l'article à supprimer
-    const indexToDelete = DB_ARTICLES.findIndex(article => article.id === idParam);
+    // Trouver l'article à supprimer
+    const articleToDelete = await Article.findOne({id : idParam});
 
     // Erreur : Pas d'article en base à supprimer
-    if (indexToDelete < 0 ){
+    if (!articleToDelete){
         return response.json(`Article non trouvé`);
     }
 
     // Supprimer un element/article dans la tableau à partir de l'index
-    DB_ARTICLES.splice(indexToDelete, 1);
+    await articleToDelete.deleteOne();
 
     // retourner l'article trouvé en json
     return response.json(`L'article ${idParam} a été supprimé avec succès !`);
